07e1204c3f6528c83f26303ad27cb421caeec5b1
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-22 17:00:48 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -19,7 +19,7 @@ const songs = [
     { title: "Dream On", artist: "Aerosmith", src: "Dream On.mp3" },
     { title: "No Smoke Without a Fire", artist: "Bad Company", src: "Bad Company (2).mp3" },
     { title: "Bad Company", artist: "Bad Company", src: "Bad Company (3).mp3" },
-    { title: "Iron Man", artist: "Black Sabbath", src: "Black Sabbath (3).mp3" },
+    { title: "Iron Man", artist: "Black Sabbath", src: "Black Sabbath (2).mp3" },
     { title: "I Heard It Through the Grapevine", artist: "Creedance Clearwater Revival", src: "Creedance Clearwater Revival (2).mp3" },
     { title: "Dream Weaver", artist: "Gary Wright", src: "Gary Wright.mp3" },
     { title: "Child in Time", artist: "Deep Purple", src: "Deep Purple (1).mp3" },
