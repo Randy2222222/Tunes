b70791a280cd92de70eed82ee363500f5691d981
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-22 05:16:55 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -16,7 +16,7 @@ const songs = [
     { title: "Mary Had A Little Lamb", artist: "Stevie Ray Vaughan", src: "Mary Had A Little Lamb.mp3" },
     { title: "Pimk Floyd Animals", artist: "Pink Floyd", src: "Pink Floyd 1977 Animals.mp3" },
     { title: "The Sound of Silence", artist: "Simon And Garfunkel", src: "Simon And Garfunkel - The Sound Of Silence.mp3" },
-    { title: "Dream On", artist: "Aerosmith", src: "Aerosmith (1).mp3" },
+    { title: "Dream On", artist: "Aerosmith", src: "Aerosmith-Dream On.mp3" },
     { title: "", artist: "Bad Company", src: "Bad Company (2).mp3" },
     { title: "", artist: "Bad Company", src: "Bad Company (3).mp3" },
     { title: "", artist: "Black Sabbath", src: "Black Sabbath (3).mp3" },
