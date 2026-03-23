0c48c63bdf751ed2b6a333709d5e7ac688a708c8
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-23 02:30:35 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -22,7 +22,7 @@ const songs = [
     { title: "Iron Man", artist: "Black Sabbath", src: "Black Sabbath (2).mp3" },
     { title: "I Heard It Through the Grapevine", artist: "Creedance Clearwater Revival", src: "Creedance Clearwater Revival (2).mp3" },
     { title: "Dream Weaver", artist: "Gary Wright", src: "Gary Wright.mp3" },
-    { title: "Child in Time", artist: "Deep Purple", src: "Deep Purple (1).mp3" },
+    { title: "Soldier of Fortune", artist: "Deep Purple", src: "Deep Purple (1).mp3" },
     { title: "Johnny Winter's Dead July-17 23-30-51", artist: "Johhny Winter's", src: "JOHNNY WINTER'S DEAD-July 17 23-30-51.mp3" },
     { title: "Me and Bobby Mcgee", artist: "Janis Joplin", src: "Janis Joplin.mp3" },
     { title: "When the Levee Breaks", artist: "Led Zeppelin", src: "Led Zeppelin.mp3" },
