1f80f0ea9c2fad36c3bf64552b1366df01588286
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-24 14:00:58 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -49,7 +49,7 @@ const songs = [
     { title: "Working for MCA", artist: "Lynyrd Skynyrd", src: "Songs/lynyrd skynyrd (2).mp3" },
     { title: "Takin' It To The Streets", artist: "The Doobie Brothers", src: "Songs/The Doobie Brothers.mp3" },
     { title: "Sittin' On The Dock Of The Bay", artist: "Otis Redding", src: "Songs/Sittin' On The Dock Of The Bay Otis Redding.Osts Platoon.mp3" },
-    { title: "Sara", artist: 'Fleetwood Mac", src: "Songs/Sara(Fleetwood Mac cover).mp3" },
+    { title: "Sara", artist: "Fleetwood Mac", src: "Songs/Sara(Fleetwood Mac cover).mp3" },
     { title: "Alabama Song", artist: "The Doors", src: "Songs/The Doors (4).mp3" }
 ];
 
