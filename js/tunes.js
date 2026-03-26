a3aa88f997335c62401cc3880bce50a6ad56a021
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-26 03:49:22 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -75,9 +75,9 @@ const songs = [
     { title: "Ghost Song", artist: "The Doors", src: "Songs/Ghost Song.mp3" },
     { title: "Janie's Got A Gun", artist: "Aerosmith", src: "Songs/Aerosmith (2).mp3" },
     { title: "Sweet Emotions", artist: "Aerosmith", src: "Songs/Aerosmith.mp3" },
-    { title: "Schools Out", artist: "Alice Cooper", src: "Songs/Alice Cooper- Schools Out.mp3" },
-    { title: "Welcom To My Nightmare", artist: "Alice Cooper", src: "Songs/Alice Cooper - Welcome To My Nightmare.mp3" },
-    { title: "Hey Stoopid", artist: "Alice Cooper", src: "Songs/Alice Cooper - Hey Stoopid 2.mp3" },
+    { title: "Schools Out", artist: "Alice Cooper", src: "Songs/Alice Cooper - Schools Out.mp3" },
+    { title: "Welcom To My Nightmare", artist: "Alice Cooper", src: "Songs/Alice Cooper - Welcome to My Nightmare.mp3" },
+    { title: "Hey Stoopid", artist: "Alice Cooper", src: "Songs/Alice Cooper - Hey Stoopid.mp3" },
     { title: "Caffiene", artist: "Alice Cooper", src: "Songs/Alice Cooper - Caffeine.mp3" },
     { title: "Eighteen", artist: "Alice Cooper", src: "Songs/Alice Cooper - Eighteen 2.mp3" }
 ];
