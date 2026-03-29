ca61f1cc3c00ff58929529901fb8c4d41b34ecfd
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-29 18:44:23 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -180,7 +180,9 @@ const songs = [
     { title: "Soul Man", artist: "Blues Brothers", src: "Songs/blues brothers.mp3" },
     { title: "Have You Ever Seen The Rain", artist: "Creedence Clearwater Revival", src: "Songs/Creedence Clearwater Revival (6).mp3" },
     { title: "Roadhouse Blues(Live)", artist: "The Doors", src: "Songs/The Doors.mp3" },
-    { title: "The Wasp", artist: "The Doors", src: "Songs/The Doors (1).mp3" }
+    { title: "The Wasp", artist: "The Doors", src: "Songs/The Doors (1).mp3" },
+    { title: "Wanted Dead or Alive", artist: "Bon Jovi", src: "Songs/Wanted Dead or Alive.mp3" }
+    
         
         
        
