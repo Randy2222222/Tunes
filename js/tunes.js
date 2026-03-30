aa606cd171f6cfd209575c2de7d3506550b7e69b
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-29 23:12:20 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -186,7 +186,8 @@ const songs = [
     { title: "Blaze of Glory", artist: "Jon Bon Jovi", src: "Songs/Blaze of Glory.mp3" },
     { title: "Smokin' In the Boys Room", artist: "Brownsville Station", src: "Songs/Smokin' In The Boys Room.mp3" },
     { title: "In The Air Tonight (Live)", artist: "Phil Collins", src: "Songs/In The Air Tonight (Live).mp3" },
-    { title: "Oh, Pretty Woman (Live, Black ‘n’ White Night)", artist: "Roy Orbison", src: "Songs/Oh, Pretty Woman (Live Black ‘n’ White Night).mp3" }
+    { title: "Oh, Pretty Woman (Live, Black ‘n’ White Night)", artist: "Roy Orbison", src: "Songs/Oh, Pretty Woman (Live Black ‘n’ White Night).mp3" },
+    { title: "Piano Man", artist: "Billy Joel", src: "Songs/Piano Man.mp3" }
     
     
         
