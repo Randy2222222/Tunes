512860c736089b7d62098a9bb3c9750df11d1d27
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-31 22:36:28 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -184,7 +184,7 @@ const songs = [
     { title: "The Wasp", artist: "The Doors", src: "Songs/The Doors (1).mp3" },
     { title: "Wanted Dead or Alive", artist: "Jon Bon Jovi", src: "Songs/Wanted Dead or Alive.mp3" },
     { title: "Blaze of Glory", artist: "Jon Bon Jovi", src: "Songs/Blaze of Glory.mp3" },
-    { title: "Smokin' In the Boys Room", artist: "Brownsville Station", src: "Songs/Smokin' in the Boys Room 2.m4a" },
+    { title: "Smokin' In the Boys Room", artist: "Brownsville Station", src: "Songs/Smokin' in the Boys Room.mp3" },
     { title: "In The Air Tonight (Live)", artist: "Phil Collins", src: "Songs/In The Air Tonight (Live).mp3" },
     { title: "Oh, Pretty Woman (Live, Black ‘n’ White Night)", artist: "Roy Orbison", src: "Songs/Pretty Woman (Black ‘n’ White Night Live).mp3" },
     { title: "Piano Man", artist: "Billy Joel", src: "Songs/Piano Man.mp3" },
