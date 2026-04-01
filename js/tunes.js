70da48a87569bc633d610406d1260828f5853d7f
Randy2222222 <randyjpollitt@yahoo.com>
2026-04-01 04:04:41 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -199,7 +199,7 @@ const songs = [
     { title: "CROSSROADS", artist: "Stevie Ray Vaughan", src: "Songs/CROSSROADS.mp3" },
     { title: "For Your Love", artist:"The Yardbirds(1965)", src: "Songs/The Yardbirds (1).mp3" },
     { title: "Lost Woman", artist: "The Yardbirds(1966)", src: "Songs/The Yardbirds.mp3" },
-    { title: "I Love Rock 'N Roll", artist: "Joan Jett & The Blackhearts", src: "Songs/I Love Rock ’N Roll-1-1.mp3" },
+    { title: "I Love Rock 'N Roll", artist: "Joan Jett & The Blackhearts", src: "Songs/I Love Rock ’N Roll.mp3" },
     { title: "Locomotive Breath", artist: "Jethro Tull", src: "Songs/Locomotive Breath.mp3" },
     { title: "La Grange", artist: "ZZ Top", src: "Songs/La Grange.mp3" },
     { title: "Wipe Out", artist: "The Surfaris", src: "Songs/Wipeout-1.mp3" },
