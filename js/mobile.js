8463ab399c3d3adc6c976c9c9269b6193234b86e
Randy2222222 <randyjpollitt@yahoo.com>
2026-04-10 00:56:29 -0400

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -185,7 +185,7 @@ const songs = [
     { title: "Roadhouse Blues(Live)", artist: "The Doors", src: "Songs/The Doors.mp3" },
     { title: "The Wasp", artist: "The Doors", src: "Songs/The Doors (1).mp3" },
     { title: "Wanted Dead or Alive", artist: "Jon Bon Jovi", src: "Songs/Wanted Dead or Alive.mp3" },
-    { title: "Blaze of Glory", artist: "Jon Bon Jovi", src: "Songs/Blaze of Glory.mp3" },
+    { title: "Blaze of Glory", artist: "Jon Bon Jovi", src: "Songs/Jon Bon Jovi - Blaze of Glory.mp3" },
     { title: "Smokin' In the Boys Room", artist: "Brownsville Station", src: "Songs/Brownsville Station - Smokin in the Boys Room.mp3" },
     { title: "In The Air Tonight (Live)", artist: "Phil Collins", src: "Songs/Phil Collins - In The Air Tonight.mp3" },
     { title: "Oh, Pretty Woman (Live, Black ‘n’ White Night)", artist: "Roy Orbison", src: "Songs/Pretty Woman (Black ‘n’ White Night Live).mp3" },
