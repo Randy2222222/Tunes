2255ff1268fdb08f62b8929f0115daf2e4d4be98
Randy2222222 <randyjpollitt@yahoo.com>
2026-05-19 18:19:36 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -9,6 +9,7 @@ const trackArtist = document.getElementById('track-artist');
 const playlistElement = document.getElementById('playlist');
 // add const downloadLink
 const downloadLink = document.getElementById('download-link');
+const thumbnail = document.getElementById('thumbnail');
 
 
 
@@ -253,6 +254,7 @@ function loadSong(song) {
     audio.src = song.src;
     trackName.textContent = song.title;
     trackArtist.textContent = song.artist;
+    thumbnail.img = cover.src;
     
     // Blob Download 🤨/ Set Song Name for download/ update download link for current Song
     downloadLink.onclick = async (e) => {
