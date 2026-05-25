d7c7ae8c2204a411611c87e4566a3568db533936
Randy2222222 <randyjpollitt@yahoo.com>
2026-05-25 15:25:30 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -227,11 +227,11 @@ const source = audioContext.createMediaElementSource(audio);
 // Bass
 const bass = audioContext.createBiquadFilter();
 bass.type = "lowshelf";
-bass.frequency.value = 220;
+bass.frequency.value = 200;
 // Mid
 const mid = audioContext.createBiquadFilter();
 mid.type = "peaking";
-mid.frequency.value = 1200;
+mid.frequency.value = 1000;
 mid.Q.value = 1;
 // Treble
 const treble = audioContext.createBiquadFilter();
