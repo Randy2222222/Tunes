56d7955c5f3dc2df4a71259f69065d11f60037ef
Randy2222222 <randyjpollitt@yahoo.com>
2026-05-25 14:57:18 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -227,7 +227,7 @@ const source = audioContext.createMediaElementSource(audio);
 // Bass
 const bass = audioContext.createBiquadFilter();
 bass.type = "lowshelf";
-bass.frequency.value = 200;
+bass.frequency.value = 80;
 // Mid
 const mid = audioContext.createBiquadFilter();
 mid.type = "peaking";
