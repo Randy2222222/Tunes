3e9e10f179549ced5c0af43712099d0c2f7e03b8
Randy2222222 <randyjpollitt@yahoo.com>
2026-06-17 12:07:46 -0400

    Update tunes.js

--- js/tunes.js
+++ js/tunes.js
@@ -7,6 +7,7 @@ const volumeControl = document.getElementById('volume');
 const trackName = document.getElementById('track-name');
 const trackArtist = document.getElementById('track-artist');
 const downloadLink = document.getElementById('download-link');
+const artwork = document.getElementById('artwork-title');
 
 
 const songs = [
@@ -280,6 +281,9 @@ navigator.mediaSession.setActionHandler('previoustrack', previousSong);
         navigator.mediaSession.metadata = new MediaMetadata({
             title: song.title,
             artist: song.artist,
+
+artwork: [
+            { src: "artwork/title.png", sizes: "96x96", type: "image/png" }]
             
         
         });
